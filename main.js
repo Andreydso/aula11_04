24eab49e2eaba67c07dabc5f9fc2abec64b2ec8e
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-04-11 22:32:04 -0300

    fezes

---
+++ main.js
@@ -0,0 +1,16 @@
+// let resposta = confirm("Deseja continuar?")
+// console.log(resposta);
+
+// let idade = prompt("Qual a sua idade?");
+
+// if (idade >= 18) {
+//     console.log("Você é maior de idade!");
+// }else{
+//     console.log("Pivete!");
+// }
+
+
+let x = prompt("Informe o calor de x");
+let y = prompt("Informe o calor de y");
+
+console.log(x + " + " +  y + " = " + (parseInt(x) + parseInt(y)));
